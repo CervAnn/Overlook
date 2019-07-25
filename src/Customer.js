3768cd8d88b10f820acaaacb969ab34cff2ffd63
Ann Cerveny <cervann11@gmail.com>
2019-07-25 17:19:27 -0600

    Finish setting up initial tests for Hotel

--- src/Customer.js
+++ src/Customer.js
@@ -1,6 +1,8 @@
 class Customer {
-  constructor() {
-
+  constructor(hotel, id, name) {
+    this.hotel = hotel;
+    this.id = id;
+    this.name = name;
   }
 }
 
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,20 +1,26 @@
-import chai from 'chai';
+import chai from "chai";
 const expect = chai.expect;
 // import userData = '../sampleData/userData'
 // import roomData = '../sampleData/roomData'
 // import bookingsData = '../sampleData/bookingsData'
 // import roomServicesData = '../sampleData/roomServicesData'
-import Customer from '../src/Customer'
+import Customer from "../src/Customer";
+import hotel from "../src/Hotel";
 
-describe('Customer', function () {
-
-
-  it('should be a function', () => {
-    expect(Customer).to.be.a('function');
+describe("Customer", function() {
+  it("should be a function", () => {
+    expect(Customer).to.be.a("function");
   });
 
-  it('should be an instance of Hotel', () => {
-    let customer = new Customer()
+  it("should be an instance of Hotel", () => {
+    let customer = new Customer(hotel, 7, "Josianne Huels");
     expect(customer).to.be.an.instanceof(Customer);
   });
-})
+
+  it("should establish name and id properties", () => {
+    let customer = new Customer(hotel, 7, "Josianne Huels");
+    expect(customer).to.not.equal(undefined);
+    expect(customer.id).to.equal(7);
+    expect(customer.name).to.equal("Josianne Huels");
+  });
+});
