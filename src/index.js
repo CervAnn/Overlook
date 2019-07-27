d6d4e1c3e6596b03d82d96afad2b6531f8a92fdc
Ann Cerveny <cervann11@gmail.com>
2019-07-27 13:08:30 -0600

    Complete today room availability method

--- src/index.js
+++ src/index.js
@@ -14,39 +14,54 @@ let roomData;
 let roomServicesData;
 
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
-.then(response => response.json())
-.then(data => customerData = data.users)
+  .then(response => response.json())
+  .then(data => customerData = data.users)
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
-.then(response => response.json())
-.then(data => bookingsData = data.bookings)
+  .then(response => response.json())
+  .then(data => bookingsData = data.bookings)
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
-.then(response => response.json())
-.then(data => roomData = data.rooms)
+  .then(response => response.json())
+  .then(data => roomData = data.rooms)
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices")
-.then(response => response.json())
-.then(data => roomServicesData = data.roomServices)
+  .then(response => response.json())
+  .then(data => roomServicesData = data.roomServices)
 
 let currentDay = new Date();
 let date = currentDay.getDate();
 let month = currentDay.getMonth() + 1;
 let year = currentDay.getFullYear();
-if (date < 10) {date = 0 + date};
-if (month < 10) {month = 0 + month};
-const today = year + "/" + month + "/" + date
+if (date < 10) {date = "0" + date}
+if (month < 10) {month = "0" + month}
+let today = year + "/" + month + "/" + date
 
 
 
 $(document).ready(function() {
-setTimeout(function() {
-    const hotel = new Hotel(customerData, bookingsData, roomData, roomServicesData)
-    console.log(hotel.bookingsData)
+  setTimeout(function() {
     $('.splash-page').hide()
     $('.main-page-container').removeAttr('hidden')
-    
-    availableRoomsToday(hotel)
+    availableRoomsToday()
 
-    function availableRoomsToday(hotel) {
-        console.log(hotel.bookingsData.filter(item => item.date === today))
-      }
-  }, 2000)
+    function availableRoomsToday() {
+      let numOccupiedRooms = bookingsData.filter(item => item.date === today);
+      console.log(roomData.length - numOccupiedRooms.length)
+    }
+
+    function totalRevenueToday() {
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}, 3000)  
 })
