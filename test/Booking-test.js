ab4b80b230065de16b3add2d54d86ba76dd1a156
Ann Cerveny <cervann11@gmail.com>
2019-07-28 20:31:21 -0600

    Complete Booking test refactor

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,20 +1,40 @@
 import chai from 'chai';
 const expect = chai.expect;
-// import userData = '../sampleData/userData'
-// import roomData = '../sampleData/roomData'
-// import bookingsData = '../sampleData/bookingsData'
-// import roomServicesData = '../sampleData/roomServicesData'
+import bookingsData from '../sampleData/bookingsData'
 import Booking from '../src/Booking'
 
+let randomIndex = Math.floor(Math.random() * (bookingsData.bookings.length - 1)) + 1;
+let randomCustomerID = bookingsData.bookings[randomIndex].userID
+let randomDate = bookingsData.bookings[randomIndex].date
+let randomRoomNumber = bookingsData.bookings[randomIndex].roomNumber
+
 describe('Booking', function () {
-
-
-  it('should be a function', () => {
-    expect(Booking).to.be.a('function');
+  
+  it("should be a function", () => {
+    expect(Booking).to.be.a("function");
+  });
+  
+  let booking;
+  beforeEach(() => {
+    booking = new Booking(randomCustomerID, randomDate, randomRoomNumber);
+  });
+  
+  it("should be an instance of Booking", () => {
+    expect(booking).to.be.an.instanceof(Booking);
+  });
+  
+  it("should establish an id property", () => {
+    expect(booking).to.not.equal(undefined);
+    expect(booking.userID).to.equal(randomCustomerID);
+  });
+  
+  it("should establish a date property", () => {
+    expect(booking).to.not.equal(undefined);
+    expect(booking.date).to.equal(randomDate);
   });
 
-  it('should be an instance of Booking', () => {
-    let booking1 = new Booking()
-    expect(booking1).to.be.an.instanceof(Booking);
+  it("should establish a room number property", () => {
+    expect(booking).to.not.equal(undefined);
+    expect(booking.roomNumber).to.equal(randomRoomNumber);
   });
 })
